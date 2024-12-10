0cc292c1a282e0bec728bcae238b84849bc3a957
[PeterWolf242] <[peter.ebm@web.de]>
2024-12-10 15:59:36 +0100

    Functions

---
+++ Functions-Level-1_2/assets/js/main.js
@@ -0,0 +1,12 @@
+
+function intro2(paramName) {
+    let Name = "SuperCoder:in";
+    let Name2 = "Developer"
+    console.log("Hi " + Name + ". Mein Name ist " + paramName);
+    console.log("Hi " + Name2 + ". Mein Name ist " + paramName);
+}
+
+intro2("Peter");
+
+intro2("Klaus Hübenbecker")
+
---
+++ Functions-Level-1_3/assets/js/main.js
@@ -0,0 +1,7 @@
+function intro3(name, stadt, alter) {
+    console.log("Hallo, mein Name ist " + name + ". Ich bin " + alter + " alt. Ich komme aus " + stadt);
+}
+
+intro3("Peter", "Ottersweier", "48");
+
+intro3("Klaus Hübenbecker", "Werner´s Schaltzentrale", "17");
